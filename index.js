ab067366516b82e8fba777703f9cc09634c1958b
MarcalFilosofo <58859672+MarcalFilosofo@users.noreply.github.com>
2022-01-01 09:05:05 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const mailer = require('nodemailer');
 
 const appointmentService = require('./services/AppointmentService');
 
